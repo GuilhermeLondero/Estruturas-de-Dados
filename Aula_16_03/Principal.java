2836aace89a5e9a2d0d80119e9c856b38f66fb78
GuilhermeLondero <londero.guilherme@ufn.edu.br>
2026-03-16 22:06:56 +0000

    Python to Java

---
+++ Aula_16_03/Principal.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        ArrayList<Aluno> alunos = new ArrayList<>();
+        
+        Scanner teclado = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Digite o nome do aluno: ");
+            String nome = teclado.nextLine().toUpperCase();
+            Aluno aluno = new Aluno(nome);
+            if (!alunos.contains(aluno)) {
+                alunos.add(aluno);
+            }
+        }
+
+        alunos.sort((a1,a2) -> a1.nome.compareTo(a2.nome));
+        
+        
+        for (Aluno aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
